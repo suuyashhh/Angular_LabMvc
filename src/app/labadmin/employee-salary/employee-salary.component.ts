9ece79a9c5b1d119af192a9e0f6280a8d846fd7a
suuyashhh <144991357+suuyashhh@users.noreply.github.com>
2025-06-16 08:43:43 +0530

    Done Until EmployeeSalary

--- src/app/labadmin/employee-salary/employee-salary.component.ts
+++ src/app/labadmin/employee-salary/employee-salary.component.ts
@@ -8,45 +8,50 @@ import { Modal } from 'bootstrap';
 @Component({
   selector: 'app-employee-salary',
   standalone: true,
-  imports: [HttpClientModule,ReactiveFormsModule,CommonModule,FormsModule],
+  imports: [HttpClientModule, ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './employee-salary.component.html',
   styleUrl: './employee-salary.component.css'
 })
 export class EmployeeSalaryComponent {
-employeesalary:any;
-    data:any;
-  
-    constructor(private api:ApiService){}
-    ngOnInit(): void {
-      this.data = new FormGroup({
-        EMP_TRN_ID : new FormControl(),
-        EMP_PRICE : new FormControl(),
-        DATE: new FormControl()
-      });
-      this.load();
-    }
+  employeesalary: any;
+  data: any;
+  employee: any;
 
-    
-        load(){
-          this.api.get('EmployeeSalary/EmployeeSalary').subscribe((res:any)=>{
-            this.employeesalary=res;
-            console.log(this.employeesalary)
-          })
-      
-        }
-      
-        submit(employeesalary:any){
-          console.log(employeesalary);
-          this.api.post('EmployeeSalary/SaveEmployeeSalary',employeesalary).subscribe((res:any)=>{
-            console.log(res);
-            this.load();
-      
-            const modalElement = document.getElementById('myModal');
-          if(modalElement){
-            const modal = Modal.getInstance(modalElement);
-            modal?.hide();
-          }
-          
-          })
-        }
+  constructor(private api: ApiService) { }
+  ngOnInit(): void {
+    this.data = new FormGroup({
+      EMP_TRN_ID: new FormControl(),
+      EMP_PRICE: new FormControl(),
+      DATE: new FormControl()
+    });
+    this.load();
+  }
+
+
+  load() {
+    this.api.get('EmployeeSalary/EmployeeSalary').subscribe((res: any) => {
+      this.employeesalary = res;
+      console.log(this.employeesalary)
+    });
+    this.api.get('Employee/Employees').subscribe((res: any) => {
+      this.employee = res;
+      console.log(this.employee);
+    });
+
+  }
+
+  submit(employeesalary: any) {
+    console.log(employeesalary);
+    this.api.post('EmployeeSalary/SaveEmployeeSalary', employeesalary).subscribe((res: any) => {
+      console.log(res);
+      this.load();
+
+      const modalElement = document.getElementById('myModal');
+      if (modalElement) {
+        const modal = Modal.getInstance(modalElement);
+        modal?.hide();
+      }
+
+    })
+  }
 }
