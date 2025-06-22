59b5ed8a460e945803808a17384995b129b7b713
Suyash Patil <Suyash Patil@DESKTOP-I1SUO0U>
2025-06-22 12:38:12 +0530

    intial change

--- src/app/labadmin/add-test/add-test.component.ts
+++ src/app/labadmin/add-test/add-test.component.ts
@@ -7,68 +7,123 @@ import { log } from 'console';
 import { HttpClientModule } from '@angular/common/http';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { Modal } from 'bootstrap';
+import bootstrap, { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-add-test',
   standalone: true,
-  imports: [HttpClientModule,ReactiveFormsModule,CommonModule,FormsModule],
+  imports: [HttpClientModule, ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './add-test.component.html',
   styleUrl: './add-test.component.css'
 })
 export class AddTestComponent implements OnInit {
 
-  tests:any;
-  data:any;
-  TEST_CODE:any;
+  tests: any;
+  data: any;
+  TEST_CODE: any = 0;
+  ComId: number = 0;
+  btn: any = '';
+  submitted = false;
+  Reason: any = '';
 
-  constructor(private api:ApiService){}
+  constructor(private api: ApiService) { }
+
+
   ngOnInit(): void {
-    this.data = new FormGroup({
-      TEST_NAME: new FormControl('',Validators.compose([Validators.required])),
-      PRICE : new FormControl(),
-      LAB_PRICE: new FormControl()
-    });
     this.load();
+    this.clearData();
   }
 
+  load() {
+    this.data = new FormGroup({
+      TEST_NAME: new FormControl('', Validators.required),
+      PRICE: new FormControl('', Validators.required),
+      LAB_PRICE: new FormControl(''),
+      COM_ID: new FormControl()
+    });
 
-
-  load(){
-    this.api.get('Test/Test').subscribe((res:any)=>{
-      this.tests=res;
+    this.api.get('Test/Test').subscribe((res: any) => {
+      this.tests = res;
       console.log(this.tests)
     })
 
+    this.ComId = parseInt(localStorage.getItem("COM_ID") || '0');
+
+  }
+
+  clearData() {
+    this.TEST_CODE = 0;
+    this.btn = '';
+    this.data.patchValue({
+      TEST_NAME: '',
+      PRICE: '',
+      LAB_PRICE: ''
+    })
   }
 
   submit(test: any) {
-    console.log(test);
-    if(this.TEST_CODE == null){
-      this.api.post('Test/SaveTest', test).subscribe((res: any) => {
-        console.log(res);
+    this.submitted = false;
+    if (!this.data.valid) {
+      this.submitted = true;
+      return;
+    }
+    if (this.TEST_CODE == 0 && this.btn == '') {
+      test.COM_ID = this.ComId
+      //   this.api.post('Test/SaveTest', test).subscribe((res: any) => {
+      //     this.load();
+      //  }); 
+      // Ensure this runs after the modal is shown at least once
+const modalElement = document.getElementById('CreateFormModal');
+
+if (modalElement) {
+  // Step 1: Create or get modal instance
+  const modal = bootstrap.Modal.getInstance(modalElement) || new bootstrap.Modal(modalElement);
+
+  // Step 2: Hide the modal using Bootstrap API
+  modal.hide();
+
+  // Step 3: Wait for the modal to finish hiding and then cleanup
+  modalElement.addEventListener('hidden.bs.modal', () => {
+    // Remove any leftover backdrop manually (failsafe)
+    const backdrops = document.querySelectorAll('.modal-backdrop');
+    backdrops.forEach(b => b.remove());
+
+    // Remove body lock styles
+    document.body.classList.remove('modal-open');
+    document.body.style.overflow = '';
+    document.body.style.paddingRight = '';
+  }, { once: true }); // add listener only once
+}
+
+
+
+      console.log("post");
+
+    } else if (this.TEST_CODE != 0 && this.btn == 'E') {
+      this.api.post('Test/EditTest/' + this.TEST_CODE, test).subscribe((res: any) => {
         this.load();
-
-        const modalElement = document.getElementById('myModal');
-        if (modalElement) {
-          const modal = Modal.getInstance(modalElement);
-          modal?.hide();
-        }
-
       });
-    }else{
-       this.api.post('Test/EditTest/'+ this.TEST_CODE,test).subscribe((res: any) => {
-        console.log(res);
-        this.load();
+    }
+    else if (this.TEST_CODE != 0 && this.btn == 'D') {
+      console.log(this.Reason);
 
-      });
+      if (this.Reason != '') {
+        this.api.delete('Test/DeleteTest/' + this.TEST_CODE).subscribe((res: any) => {
+          this.load();
+        })
+      }
+      else {
+        alert("Fill the reason");
+      }
     }
   }
 
-  edit(testCode: any) {
+  getDataById(testCode: number, btn: any) {
+    // alert('Hii');
+    this.btn = btn;
     this.api.get('Test/Test/' + testCode).subscribe((res: any) => {
       console.log(res);
-      
+
       this.TEST_CODE = res.tesT_CODE;
       this.data.patchValue({
         TEST_NAME: res.tesT_NAME,
@@ -78,13 +133,34 @@ export class AddTestComponent implements OnInit {
     })
   }
 
-  deleteTest(testCode:any){
-    if(confirm("Are You Delete?")){
-      this.api.delete('Test/DeleteTest/' + testCode).subscribe((res: any) => {
-       this.load();
-      })
-    }
-  }
+  // edit(testCode: any) {
+  //   this.api.get('Test/Test/' + testCode).subscribe((res: any) => {
+  //     console.log(res);
+
+  //     this.TEST_CODE = res.tesT_CODE;
+  //     this.data.patchValue({
+  //       TEST_NAME: res.tesT_NAME,
+  //       PRICE: res.price,
+  //       LAB_PRICE: res.laB_PRICE
+  //     })
+  //   })
+  // }
+
+  // deleteTest(testCode:any){
+  //   this.api.get('Test/Test/' + testCode).subscribe((res: any) => {
+  //     console.log(res);
+
+  //     this.TEST_CODE = res.tesT_CODE;
+  //     this.data.patchValue({
+  //       TEST_NAME: res.tesT_NAME,
+  //       PRICE: res.price,
+  //       LAB_PRICE: res.laB_PRICE
+  //     })
+  //   })
+  //     this.api.delete('Test/DeleteTest/' + testCode).subscribe((res: any) => {
+  //      this.load();
+  //     })
+  // }
 
 }
 
@@ -103,9 +179,9 @@ export class AddTestComponent implements OnInit {
 // }
 
 
-  // ngOnInit(): void {
-  //   this.data = new FormGroup({
-  //     TEST_NAME: new FormControl('', Validators.compose([Validators.required])),
-  //     PRICE: new FormControl(),
-  //     LAB_PRICE: new FormControl()
-  //   });
+// ngOnInit(): void {
+//   this.data = new FormGroup({
+//     TEST_NAME: new FormControl('', Validators.compose([Validators.required])),
+//     PRICE: new FormControl(),
+//     LAB_PRICE: new FormControl()
+//   });
--- src/app/labadmin/doctor/doctor.component.ts
+++ src/app/labadmin/doctor/doctor.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../shared/api.service';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule,Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Modal } from 'bootstrap';
@@ -15,38 +15,94 @@ import { Modal } from 'bootstrap';
 export class DoctorComponent implements OnInit {
   data: any;
   doctor: any;
+  btn: any = '';
+  DOCTOR_CODE: any = 0;
+  ComId: number = 0;
+  submitted = false;
 
-  constructor(private api: ApiService) {}
+  constructor(private api: ApiService) { }
+
+  // ngOnInit(): void {
+  //   this.data = new FormGroup({
+  //     DOCTOR_NAME : new FormControl,
+  //     DOCTOR_ADDRESS: new FormControl,
+  //     DOCTOR_NUMBER : new FormControl
+  //   });
+  //   this.load();
+  // }
 
   ngOnInit(): void {
-    this.data = new FormGroup({
-      DOCTOR_NAME : new FormControl,
-      DOCTOR_ADDRESS: new FormControl,
-      DOCTOR_NUMBER : new FormControl
-    });
     this.load();
+
   }
 
   load() {
+    this.data = new FormGroup({
+      DOCTOR_NAME: new FormControl('', Validators.compose([Validators.required])),
+      DOCTOR_ADDRESS: new FormControl(),
+      DOCTOR_NUMBER: new FormControl(),
+      COM_ID: new FormControl()
+    });
+
     this.api.get('Doctor/Doctor').subscribe((res: any) => {
       this.doctor = res;
       console.log(this.doctor);
     });
+
+    this.ComId = parseInt(localStorage.getItem("COM_ID") || '0');
+
   }
 
-  submit(doctors:any){
-      console.log(doctors);
-      this.api.post('Doctor/SaveDoctor',doctors).subscribe((res:any)=>{
-        console.log(res);
-        this.load();
-  
-        const modalElement = document.getElementById('myModal');
-      if(modalElement){
-        const modal = Modal.getInstance(modalElement);
-        modal?.hide();
-      }
-      
+  clearData() {
+    this.DOCTOR_CODE = 0;
+    this.btn = '';
+    this.data.patchValue({
+      DOCTOR_NAME: '',
+      DOCTOR_ADDRESS: '',
+      DOCTOR_NUMBER: ''
+    })
+  }
+
+  submit(doctors: any) {
+     this.submitted = true;
+    
+    if(this.DOCTOR_CODE == 0 && this.btn == ''){
+      doctors.COM_ID = this.ComId
+    this.api.post('Doctor/SaveDoctor', doctors).subscribe((res: any) => {
+      this.load();
+
+      // const modalElement = document.getElementById('myModal');
+      // if (modalElement) {
+      //   const modal = Modal.getInstance(modalElement);
+      //   modal?.hide();
+      // }
+
+    });
+  }else if(this.DOCTOR_CODE != 0 && this.btn == 'E'){
+       this.api.post('Doctor/EditDoctor/'+ this.DOCTOR_CODE,doctors).subscribe((res: any) => {
+         this.load();
+      });
+    }
+    else if(this.DOCTOR_CODE != 0 && this.btn == 'D'){
+      this.api.delete('Doctor/DeleteDoctor/' + this.DOCTOR_CODE).subscribe((res: any) => {
+       this.load();
       })
     }
+  }
+
+  getDataById(doctorCode: number, btn: any) {
+    // alert('Hii');
+    this.btn = btn;
+    this.api.get('Doctor/Doctor/' + doctorCode).subscribe((res: any) => {
+      console.log(res);
+
+      this.DOCTOR_CODE = res.doctoR_CODE;
+      this.data.patchValue({
+        DOCTOR_NAME: res.doctoR_NAME,
+        DOCTOR_ADDRESS: res.doctoR_ADDRESS,
+        DOCTOR_NUMBER: res.doctoR_NUMBER
+      })
+    })
+  }
 
 }
