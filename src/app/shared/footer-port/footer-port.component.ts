e8fe3208f8bd0ac63a649ab728d769e37ba2771e
suuyashhh <144991357+suuyashhh@users.noreply.github.com>
2025-08-04 07:34:20 +0530

    Portfolio Done

---
+++ src/app/shared/footer-port/footer-port.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-port',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer-port.component.html',
+  styleUrl: './footer-port.component.css'
+})
+export class FooterPortComponent {
+
+}
