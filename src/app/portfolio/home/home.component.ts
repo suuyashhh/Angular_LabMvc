61e1e2ecd707a42071bb5fbcdf78eb593ce0bde7
Suyash Patil <Suyash Patil@DESKTOP-I1SUO0U>
2025-08-12 12:27:52 +0530

    Added portfolio Footer

--- src/app/portfolio/home/home.component.ts
+++ src/app/portfolio/home/home.component.ts
@@ -1,18 +1,18 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { error } from 'node:console';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
- @ViewChild('backgroundVideo') videoRef!: ElementRef<HTMLVideoElement>;
+  @ViewChild('backgroundVideo') videoRef!: ElementRef<HTMLVideoElement>;
 
   ngOnInit() {
-    // Try to play video after component initialization
     setTimeout(() => {
       if (this.videoRef) {
         const video = this.videoRef.nativeElement;
@@ -20,9 +20,8 @@ export class HomeComponent implements OnInit {
         const playPromise = video.play();
         
         if (playPromise !== undefined) {
-          playPromise.catch((error:any) => {
+          playPromise.catch((error: any) => {
             console.log('Autoplay was prevented:', error);
-            // Show a play button or handle the case where autoplay is blocked
           });
         }
       }
